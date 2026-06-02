7a348c6bed26b73b167cd56fca7ca0bc7c4e9cc6
FarisSF97 <alfarisi97@outlook.com>
2026-06-02 10:10:18 +0700

    add see detail

--- modules/order/order_service.js
+++ modules/order/order_service.js
@@ -3,9 +3,12 @@ const helper = require("../../common/helper");
 exports.getOrders = async (account_id) => {
   try {
     const [rows] = await helper.db.query(
-      `SELECT o.id, o.invoice, o.products_id, o.qty, o.subtotal, o.diskon_jumlah, o.total, o.status, p.nama AS product_name
+       `SELECT o.id, o.invoice, o.products_id, o.harga, o.qty, o.subtotal, o.diskon_jumlah, o.total, o.status, o.created_at,
+              p.nama AS product_name, p.kode_unik,
+              b.jenis_bank AS bank_name, b.no_rek AS bank_account, b.atas_nama AS bank_owner
        FROM \`order\` o
        JOIN products p ON o.products_id = p.id
+       LEFT JOIN bank b ON o.bank_id = b.id
        WHERE o.account_id = ?
        ORDER BY o.id DESC`,
       [account_id]
